94581d3f6bffc7b614aa7da49b743c529c5b7210
jebbyk <zaicev9797@gmail.com>
2018-10-27 19:54:02 +0600

    (oop) fix lab2_3, add lab2_5

--- 3-th sem/OOP (Java)/2_5/src/Main.java
+++ 3-th sem/OOP (Java)/2_5/src/Main.java
@@ -7,17 +7,17 @@ public class Main {
     {
         String GUID = new String();
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter MAC-address!");
+        System.out.println("enter color indefier!");
         GUID = sc.nextLine();
-        Pattern pat = Pattern.compile("(([0-9|[a-f|[A-F]]]{2}):){5}([0-9|[a-f|[A-F]]]{2})");
+        Pattern pat = Pattern.compile("#[0-9|[a-f|[A-F]]]{6}");
         Matcher m = pat.matcher(GUID);
 
         if(m.matches())
         {
-            System.out.println("it's a normal MAC-address");
+            System.out.println("it's a normal color indefier");
         }else
         {
-            System.out.println("it's not a normal MAC-address");
+            System.out.println("it's not a normal color indefier");
         }
     }
 }
