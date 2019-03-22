59f760057c1a59803d3a2cf31d05c7792ab59190
jebbyk <zaicev9797@gmail.com>
2019-03-23 00:17:42 +0600

    added working with window
    printing to window and to the console in the sametime is not working

--- 4-th sem/OS(C) labs/Lab9 win/s.c
+++ 4-th sem/OS(C) labs/Lab9 win/s.c
@@ -1,23 +1,29 @@
 #include <windows.h>
 #include <process.h>
 #include <stdio.h>
+
+
 HANDLE hstdout;
 DWORD actlen;
 CRITICAL_SECTION csec;
-
-
-
 char buf[12] = "123456789012";
+HANDLE hthreadRdr1, hthreadRdr2, hthreadRdr3, hthreadWrtr1, hthreadWrtr2, hthreadWrtr3, hWinThread;
+unsigned long rdrThrd1, rdrThrd2, rdrThrd3, wrtrThrd1, wrtrThrd2, wrtrThrd3, winThread;
 
-HANDLE hthreadRdr1, hthreadRdr2, hthreadRdr3, hthreadWrtr1, hthreadWrtr2, hthreadWrtr3;
-unsigned long rdrThrd1, rdrThrd2, rdrThrd3, wrtrThrd1, wrtrThrd2, wrtrThrd3;
+HINSTANCE hInstance;
+STARTUPINFO si;
+int nCmdShow;
+HWND hwnd;
+HDC hdc;
+MSG msg;
+WNDCLASS wc;
 
 
 void procThreadWriter1(void *arg)
 {
     char pat[12] = "abcdefghijkl";
     int k;
-    for(k = 0; k < 56; k++)
+    for(k = 0; k < 32; k++)
     {
         int i;
         EnterCriticalSection(&csec);
@@ -50,7 +56,7 @@ void procThreadWriter2(void *arg)
 {
     char pat[12] = "ABCDEFGHIJKL";
     int k;
-    for(k = 0; k < 56; k++)
+    for(k = 0; k < 32; k++)
     {
         int i;
         EnterCriticalSection(&csec);
@@ -83,7 +89,7 @@ void procThreadWriter3(void *arg)
 {
     char pat[12] = "-<+_!\%^&*\\|@";
     int k;
-    for(k = 0; k < 56; k++)
+    for(k = 0; k < 32; k++)
     {
         int i;
         EnterCriticalSection(&csec);
@@ -117,8 +123,9 @@ void procThreadWriter3(void *arg)
 void procThreadReader1(void *arg)
 {
     int j;
-    for(j = 0; j < 56; j++)
+    for(j = 0; j < 32; j++)
     {
+        //*
         COORD pos;
         pos.X=20; pos.Y=j+1;
         EnterCriticalSection(&csec);
@@ -130,6 +137,11 @@ void procThreadReader1(void *arg)
             printf("%c", buf[i]);
         }
         LeaveCriticalSection(&csec);
+        //*/
+        
+        /*
+        TextOut(hdc, 20*8, j*16, buf, 12);
+        */
         Sleep(2);
     } 
 }
@@ -137,8 +149,9 @@ void procThreadReader1(void *arg)
 void procThreadReader2(void *arg)
 {
     int j;
-    for(j = 0; j < 56; j++)
+    for(j = 0; j < 32; j++)
     {
+        //*
         COORD pos;
         pos.X=40; pos.Y=j+1;
         EnterCriticalSection(&csec);
@@ -148,8 +161,14 @@ void procThreadReader2(void *arg)
         for(i = 0; i < 12; i++)
         {
             printf("%c", buf[i]);
+            
         }
         LeaveCriticalSection(&csec);
+        //*/
+
+        /*
+        TextOut(hdc, 40*8, j*16, buf, 12);
+        */
         Sleep(4);
     } 
 }
@@ -158,8 +177,9 @@ void procThreadReader3(void *arg)
 {
    
     int j;
-    for(j = 0; j < 56; j++)
+    for(j = 0; j < 32; j++)
     {
+        //*
         COORD pos;
         pos.X=60; pos.Y=j+1;
         EnterCriticalSection(&csec);
@@ -171,13 +191,55 @@ void procThreadReader3(void *arg)
             printf("%c", buf[i]);
         }
         LeaveCriticalSection(&csec);
+        //*/
+
+        /*
+        TextOut(hdc, 60*8, j*16, buf, 12);
+        */
         Sleep(6);
     } 
 }
 
 void main()
 {
+    
+/*
+    LRESULT WINAPI WinProc(HWND hwnd, UINT tmsg, WPARAM wParam, LPARAM lParam)
+    {
+       switch(tmsg)
+        {
+            case WM_DESTROY:
+            {
+                PostQuitMessage(0);
+                return 0;
+            }
+        }
+        return DefWindowProc(hwnd, tmsg, wParam, lParam);
+    }
+
+    GetStartupInfo(&si);
+    if(si.dwFlags & STARTF_USESHOWWINDOW)
+    {
+        nCmdShow = si.wShowWindow;
+    }else{
+        nCmdShow = SW_SHOWDEFAULT;
+    }
+    hInstance = GetModuleHandle(NULL);
+
+    memset(&wc, 0, sizeof(wc));
+    wc.lpszClassName = "MyClass";
+    wc.lpfnWndProc = WinProc;
+    wc.hCursor=LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground=(HBRUSH)(COLOR_WINDOW+1);
+    wc.hInstance = hInstance;
    
+    if(!RegisterClass(&wc)) return;
+
+    hwnd = CreateWindow("MyClass", "OurWindow", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, hInstance, NULL);
+    hdc = GetDC(hwnd);
+
+    ShowWindow(hwnd, nCmdShow);
+    */
     hstdout=GetStdHandle(STD_OUTPUT_HANDLE);
     system("cls");
     InitializeCriticalSection(&csec);
@@ -188,9 +250,15 @@ void main()
     hthreadRdr1 = (HANDLE)_beginthreadex(NULL, 4096, procThreadReader1, (void *)2, 0,&rdrThrd1);
     hthreadRdr2 = (HANDLE)_beginthreadex(NULL, 4096, procThreadReader2, (void *)3, 0,&rdrThrd2);
     hthreadRdr3 = (HANDLE)_beginthreadex(NULL, 4096, procThreadReader3, (void *)4, 0,&rdrThrd3);
+/*
     
-    
+   while(GetMessage(&msg, 0, 0, 0))
+   {
+        DispatchMessage(&msg);
+   }
+ */  
     getchar();
+   
     DeleteCriticalSection(&csec);
 
     CloseHandle(hthreadWrtr1); CloseHandle(hthreadWrtr2); CloseHandle(hthreadWrtr3);
