79bfe2f69ad331830dee5f8707ce34a448627617
jebbyk <zaicev9797@gmail.com>
2019-03-23 22:41:27 +0600

    Lab10 OS(C) example
    not working

--- 4-th sem/OS(C) labs/Lab10win/n.c
+++ 4-th sem/OS(C) labs/Lab10win/n.c
@@ -1,8 +1,32 @@
 #include <windows.h>
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
 void main()
 {
-    printf("\nlol");
-    getchar();
+    char *pb, st[20];
+    HANDLE hmem;
+    int k;
+    hmem=OpenFileMapping(FILE_MAP_READ, FALSE, "SHAREMEM_MYY");
+    if (hmem==0)
+    {
+        printf("Error OpenSharedMem with RC=%ld\n", GetLastError());
+        getchar(); 
+        exit(0);
+    }
+    pb=MapViewOfFile(hmem, FILE_MAP_READ,0,0,0);
+    if (pb==NULL)
+    {
+        printf("Error Mapping SharedMem with RC=%ld\n", GetLastError());
+        getchar(); 
+        exit(0);
+    }
+    for (k=0;k<10;k++ )
+    {
+        strncpy(st, pb, 20); 
+        st[19]='\0'; 
+        printf("%s\n", st); 
+        Sleep(2000);
+    }
+    UnmapViewOfFile(pb); CloseHandle(hmem);
 }
--- 4-th sem/OS(C) labs/Lab10win/s.c
+++ 4-th sem/OS(C) labs/Lab10win/s.c
@@ -1,14 +1,32 @@
-#define _WIN32_WINNT 0x0500
 #include <windows.h>
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
 void main()
 {
-
-
-    if (CreateProcess("n.exe","child",NULL,NULL,TRUE,CREATE_NEW_CONSOLE,NULL,NULL,&cif,&pi)==TRUE)
+    char *pm;
+    HANDLE hmem;
+    hmem=CreateFileMapping((HANDLE)0xFFFFFFFF,NULL,  PAGE_READWRITE, 0, 16000, "SHAREMEM_MYY");
+    if (hmem==0)
     {
-
+        printf("Error AllocSharedMem with RC=%ld\n", GetLastError());
+        getchar(); 
+        exit(0); 
     }
-
-}
+    pm=MapViewOfFile(hmem, FILE_MAP_WRITE,0,0,0);
+    if (pm==NULL)
+    {
+        printf("Error Mapping SharedMem with RC=%ld\n", GetLastError());
+        getchar(); 
+        exit(0);
+    }
+    Sleep(4000);
+    strcpy(pm, "Privet Shara !!!"); 
+    printf("Middle work\n"); 
+    Sleep(10000);
+    printf("Two step\n");
+    strcpy(pm+7, "- Good Bye !!!"); 
+    Sleep(10000);
+    UnmapViewOfFile(pm);
+    CloseHandle(hmem);
+} 
