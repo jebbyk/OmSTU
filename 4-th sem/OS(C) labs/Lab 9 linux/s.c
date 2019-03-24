d31e3dfb9750928e83628b91c0083ef1e7c9a938
Jebbyk-proger <zaicev9797@gmail.com>
2019-03-24 16:29:21 +0600

    Added window output for Lab9 OS(C) linux
    works good!!

--- 4-th sem/OS(C) labs/Lab 9 linux/s.c
+++ 4-th sem/OS(C) labs/Lab 9 linux/s.c
@@ -2,10 +2,24 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdlib.h>
+#include <X11/Xlib.h>
 
 
 char buf[12];
 int hardnes = 1024, calcRes;
+
+Display *dspl;
+int screen;
+Window hwnd;
+XEvent event;
+KeySym ks;
+GC gc;
+int x, y,w,h;
+int count = 0;
+int speed = 100000;
+
+
 
 
 pthread_t rdrThrd1, rdrThrd2, rdrThrd3, wrtrThrd1, wrtrThrd2, wrtrThrd3;
@@ -15,7 +29,7 @@ void procThreadWriter1(void *arg)
 {
     char pat[12] = "abcdefghijkl";
     int k;
-    for(k = 0; k < 24; k++)
+    for(k = 0; k < 32; k++)
     {
         pthread_mutex_lock(&hmtx);
         int i;
@@ -25,7 +39,7 @@ void procThreadWriter1(void *arg)
         }
         pthread_mutex_unlock(&hmtx);
 
-        usleep(1000000);
+        usleep(1*speed);
 
         pthread_mutex_lock(&hmtx);
         for(i = 6; i < 12; i++)
@@ -34,7 +48,7 @@ void procThreadWriter1(void *arg)
         }
         pthread_mutex_unlock(&hmtx);
 
-        usleep(1000000);
+        usleep(1*speed);
     }
 }
 
@@ -43,7 +57,7 @@ void procThreadWriter2(void *arg)
 {
     char pat[12] = "ABCDEFGHIJKL";
     int k;
-    for(k = 0; k < 24; k++)
+    for(k = 0; k < 32; k++)
     {
         pthread_mutex_lock(&hmtx);
         int i;
@@ -53,7 +67,7 @@ void procThreadWriter2(void *arg)
         }
         pthread_mutex_unlock(&hmtx);
 
-        usleep(1500000);
+        usleep(2*speed);
 
         pthread_mutex_lock(&hmtx);
         for(i = 6; i < 12; i++)
@@ -62,7 +76,7 @@ void procThreadWriter2(void *arg)
         }
         pthread_mutex_unlock(&hmtx);
 
-        usleep(1500000);
+        usleep(2*speed);
     }
 }
 
@@ -70,7 +84,7 @@ void procThreadWriter3(void *arg)
 {
     char pat[12] = "-<+_!\%^&*\\|@";
     int k;
-    for(k = 0; k < 24; k++)
+    for(k = 0; k < 32; k++)
     {
         pthread_mutex_lock(&hmtx);
         int i;
@@ -80,7 +94,7 @@ void procThreadWriter3(void *arg)
         }
         pthread_mutex_unlock(&hmtx);
 
-        usleep(2000000);
+        usleep(3*speed);
 
         pthread_mutex_lock(&hmtx);
         for(i = 6; i < 12; i++)
@@ -89,14 +103,14 @@ void procThreadWriter3(void *arg)
         }
         pthread_mutex_unlock(&hmtx);
 
-        usleep(2000000);
+        usleep(3*speed);
     }
 }
 
 void procThreadReader1(void *arg)
 {
     int j;
-    for(j = 0; j < 40; j++)
+    for(j = 0; j < 32; j++)
     {
         pthread_mutex_lock(&hmtx);
         printf("\033[%d;0H\033[1;34m",j+1);
@@ -106,14 +120,18 @@ void procThreadReader1(void *arg)
             printf("%c", buf[i]);
         }
         printf("\n");
+
+        XDrawString(dspl, hwnd, gc, 20*8, j*16,buf, 12);
+        XFlush(dspl);
+
         pthread_mutex_unlock(&hmtx);
-        usleep(150000);
+        usleep(2*speed);
     } 
 }
 void procThreadReader2(void *arg)
 {
     int j;
-    for(j = 0; j < 40; j++)
+    for(j = 0; j < 32; j++)
     {
          pthread_mutex_lock(&hmtx);
         printf("\033[%d;30H\033[1;34m",j+1);
@@ -123,14 +141,18 @@ void procThreadReader2(void *arg)
             printf("%c", buf[i]);
         }
         printf("\n");
+
+        XDrawString(dspl, hwnd, gc, 40*8, j*16,buf, 12);
+        XFlush(dspl);
+
         pthread_mutex_unlock(&hmtx);
-        usleep(300000);
+        usleep(4*speed);
     } 
 }
 void procThreadReader3(void *arg)
 {
     int j;
-    for(j = 0; j < 40; j++)
+    for(j = 0; j < 32; j++)
     {
          pthread_mutex_lock(&hmtx);
         printf("\033[%d;60H\033[1;34m",j+1);
@@ -140,14 +162,27 @@ void procThreadReader3(void *arg)
             printf("%c", buf[i]);
         }
         printf("\n");
+
+        XDrawString(dspl, hwnd, gc, 60*8, j*16,buf, 12);
+        XFlush(dspl);
+
         pthread_mutex_unlock(&hmtx);
-        usleep(600000);
+        usleep(6*speed);
     } 
 }
 
 
 void main()
 {
+
+    dspl = XOpenDisplay(NULL);
+    gc = XDefaultGC(dspl,0);
+    if(dspl == 0) {printf("Error XOpenDisplay\n"); exit(1);}
+    screen = XDefaultScreen(dspl);
+    hwnd = XCreateSimpleWindow(dspl, RootWindow(dspl, screen), 100,50,1280,720,3, BlackPixel(dspl,screen), WhitePixel(dspl,screen));
+    if(hwnd == 0) {printf("Error XCreateSimpleWindow\n"); exit(1);}
+    XSelectInput(dspl, hwnd, ExposureMask | KeyPressMask | ButtonPressMask | StructureNotifyMask);//input mask
+    XMapWindow(dspl, hwnd);
 
     int rc;
     printf("\033[2J\n");//cls
@@ -159,6 +194,5 @@ void main()
     rc=pthread_create(&rdrThrd2, NULL, (void*)procThreadReader2, (void*)2);
     rc=pthread_create(&rdrThrd3, NULL, (void*)procThreadReader3, (void*)2);
 
- 
     getchar(); printf("\033[0m");
 }
