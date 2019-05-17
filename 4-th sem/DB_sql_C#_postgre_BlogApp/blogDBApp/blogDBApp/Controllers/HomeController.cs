1d21c4089db928d9b25a686f06225d1e27f25531
jebbyk <zaicev9797@gmail.com>
2019-05-18 00:34:39 +0600

    BlogDBApp (C# .NET Postgre)
    -extra requests

--- 4-th sem/DB_sql_C#_postgre_BlogApp/blogDBApp/blogDBApp/Controllers/HomeController.cs
+++ 4-th sem/DB_sql_C#_postgre_BlogApp/blogDBApp/blogDBApp/Controllers/HomeController.cs
@@ -7,6 +7,9 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using blogDBApp.Models;
+using blogDBApp.RequestsClasses;
+using Npgsql;
+using NpgsqlTypes;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
@@ -15,6 +18,7 @@ namespace blogDBApp.Controllers
 {
     public class HomeController : Controller
     {
+        private blogDataBaseEntities2 db = new blogDataBaseEntities2();
         public ActionResult Index()
         {
 
@@ -32,5 +36,181 @@ namespace blogDBApp.Controllers
 
             return View();
         }
+
+        public ActionResult Requests()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult Request1(string name)//вывести коменты пользователь с ненуллевым рейтингом
+        {
+            /*ViewBag.result = db.comments.Where(c => c.users.name == name).
+                Where(c => c.rating > 0).ToList();*/
+
+            using (var db = new blogDataBaseEntities2())
+            {
+                var param = new NpgsqlParameter("@nm", name)
+                {
+                    NpgsqlDbType = NpgsqlDbType.Varchar
+                };
+                string sql = @" select u.name, u.id, c.text, c.rating
+                                from comments c
+                                inner join users u
+                                on u.id = c.user
+                                and u.name = @nm
+                                and c.rating >0;
+                                ";
+                ViewBag.result = db.Database.SqlQuery<Request1Columns>(sql, param).ToList();
+            }
+
+
+            return View();
+        }
+ 
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Request2()//коментарии добавленные после создания последней публикации
+        {
+            using (var db = new blogDataBaseEntities2())
+            {
+                string sql = @" select u.id, u.name, c.text, c.date
+                                from comments c
+                                join users u
+                                on c.user = u.id
+                                where c.date > (select max(p.date)   
+                                from publications p)
+                                ;";
+                ViewBag.result = db.Database.SqlQuery<Request2Columns>(sql).ToList();
+            }
+
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Request3()//кол. коментов в каждой теме
+        {
+            //List<Request3Columns> result;
+            using (var db = new blogDataBaseEntities2())
+            {
+                string sql = @" select t.name, t.id, count(*) 
+                                from publications p
+                                join comments c
+                                on c.publication = p.id
+                                join themes t
+                                on t.id = p.theme
+                                group by t.id
+                                order by t.id
+                                ;";
+                ViewBag.result = db.Database.SqlQuery<Request3Columns>(sql).ToList();
+            }
+
+                return View();
+        }
+
+        public ActionResult Request4(string theme)//пользователи создавших публикации на тему 
+        {
+            using (var db = new blogDataBaseEntities2())
+            {
+                var param = new NpgsqlParameter("@thm", theme)
+                {
+                    NpgsqlDbType = NpgsqlDbType.Varchar
+                };
+                string sql = @" select u.name as ""uName"", u.id as ""uID"", p.name as ""pName"", p.id as ""pID"", t.name as ""tName"", t.id as ""tID""
+                                from users u
+                                join publications p
+                                on u.id = p.user
+                                join themes t
+                                on p.theme = t.id
+                                where t.name = @thm;
+                                ";
+                ViewBag.result = db.Database.SqlQuery<Request45Columns>(sql, param).ToList();
+            }
+
+
+
+            return View();
+        }
+
+        public ActionResult Request5(string theme1, string theme2)//пользователи создавших публикации на тему 
+        {
+            using (var db = new blogDataBaseEntities2())
+            {
+                var param1 = new NpgsqlParameter("@thm1", theme1)
+                {
+                    NpgsqlDbType = NpgsqlDbType.Varchar
+                };
+                var param2 = new NpgsqlParameter("@thm2", theme2)
+                {
+                    NpgsqlDbType = NpgsqlDbType.Varchar
+                };
+                string sql = @" select u.name as ""uName"", u.id as ""uID"", p.name as ""pName"", p.id as ""pID"", t.name as ""tName"", t.id as ""tID""
+                                from users u
+                                join publications p
+                                on u.id = p.user
+                                join themes t
+                                on p.theme = t.id
+                                where t.name = @thm1
+                                union all
+                                select u.name as ""uName"", u.id as ""uID"", p.name as ""pName"", p.id as ""pID"", t.name as ""tName"", t.id as ""tID""
+                                from users u
+                                join publications p
+                                on u.id = p.user
+                                join themes t
+                                on p.theme = t.id
+                                where t.name = @thm2;
+                                ";
+                ViewBag.result = db.Database.SqlQuery<Request45Columns>(sql, param1, param2).ToList();
+            }
+            
+            return View();
+        }
+
+
+        public ActionResult Request6(string theme1, string theme2, int? rating)//коменты пользователей написавших их к темам в каких то категориях
+        {
+            using (var db = new blogDataBaseEntities2())
+            {
+                var param1 = new NpgsqlParameter("@thm1", theme1)
+                {
+                    NpgsqlDbType = NpgsqlDbType.Varchar
+                };
+                var param2 = new NpgsqlParameter("@thm2", theme2)
+                {
+                    NpgsqlDbType = NpgsqlDbType.Varchar
+                };
+                var param3 = new NpgsqlParameter("@rtng", rating)
+                {
+                    NpgsqlDbType = NpgsqlDbType.Integer
+                };
+                string sql = @" select u.name as ""uName"", u.id as ""uID"", p.name as ""pName"", p.id as ""pID"", t.name as ""tName"", t.id as ""tID"", c.rating as ""cRating"", c.text as ""cText""
+                                from publications p
+                                join comments c
+                                on c.publication = p.id
+                                join users u
+                                on u.id = c.user
+                                join themes t
+                                on t.id = p.theme
+                                where c.rating > @rtng and t.name = @thm1
+                                union all
+                                select u.name as ""uName"", u.id as ""uID"", p.name as ""pName"", p.id as ""pID"", t.name as ""tName"", t.id as ""tID"", c.rating as ""cRating"", c.text as ""cText""
+                                from publications p
+                                join comments c
+                                on c.publication = p.id
+                                join users u
+                                on u.id = c.user
+                                join themes t
+                                on t.id = p.theme
+                                where c.rating > @rtng and t.name = @thm2;
+                                ";
+                ViewBag.result = db.Database.SqlQuery<Request6Columns>(sql, param1, param2, param3).ToList();
+            }
+            return View();
+        }
+
     }
 }
