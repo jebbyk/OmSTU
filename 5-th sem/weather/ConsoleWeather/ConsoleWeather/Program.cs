80902df93d706e0bf471cd9ba2114ea3500829e8
jebbyk <zaicev9797@gmail.com>
2019-11-07 10:56:23 +0600

    create project

---
+++ 5-th sem/weather/ConsoleWeather/ConsoleWeather/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleWeather
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello there!! Weather!!!");
+        }
+    }
+}
