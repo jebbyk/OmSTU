65ddd68bbf36953aad8663c6a0cc5ab43d69a139
jebbyk <zaicev9797@gmail.com>
2019-12-09 23:30:38 +0600

    oracle

--- 5-th sem/dataBases/create_inspections_table.sql
+++ 5-th sem/dataBases/create_inspections_table.sql
@@ -1,6 +1,3 @@
-use omstu_taxes;
-go
-if exists (select * from sys.tables where name = 'inspections')
 drop table inspections;
 
 create table inspections
@@ -8,6 +5,5 @@ create table inspections
     id int not null,
     inspection_number varchar(32) not null,
     
-    constraint pk_inspection_id primary key (id),
+    constraint pk_inspection_id primary key (id)
 );
-GO
